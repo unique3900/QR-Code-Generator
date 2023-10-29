2378d49e2ee7acef407603527a0cc2152f01317c
parashar <parasharneupane3900@gmail.com>
2023-10-29 11:04:27 +0545

    Completed

--- QrCode/src/App.tsx
+++ QrCode/src/App.tsx
@@ -6,7 +6,7 @@ const App: React.FC = () => {
   const [url, setUrl] = React.useState<string>("Welcome!");
   const [color, setColor] = React.useState<string>("black");
   return (
-    <div className='w-full h-screen flex flex-col gap-5 justify-center items-center border-2  border-black shadow-md'>
+    <div className='w-full h-screen flex flex-col gap-5 justify-center items-center px-5 py-2 shadow-2xl'>
       <h1 className="text-center font-bold">QR Code Generator</h1>
 
       <QRCodeCanvas
@@ -15,10 +15,8 @@ const App: React.FC = () => {
         fgColor={color}
       />
 
-<form>
+
       <div className="space-y-12">
-
-
         <div className="border-b border-gray-900/10 pb-12">
           <h2 className="text-base font-semibold leading-7 text-gray-900">Generator Information*</h2>
           <div className="mt-5 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
@@ -30,9 +28,14 @@ const App: React.FC = () => {
                 <input
                   type="text"
                   name="first-name"
-                  id="first-name"
+                    id="first-name"
+                    value={url}
+                    onChange={(e:React.ChangeEvent<HTMLInputElement>) => {
+                      setUrl(e.target.value)
+                      
+                    }}
                   autoComplete="given-name"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
               </div>
@@ -46,7 +49,11 @@ const App: React.FC = () => {
                   type="color"
                   name="first-name"
                   id="first-name"
-                  autoComplete="given-name"
+                    autoComplete="given-name"
+                    value={color}
+                    onChange={(e:React.ChangeEvent<HTMLInputElement>) => {
+                      setColor(e.target.value)
+                    }}
                   className="block w-10 rounded-md border-0  text-gray-900  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -56,16 +63,8 @@ const App: React.FC = () => {
           </div>
         </div>
       </div>
-      <div className="mt-6 flex items-center justify-end gap-x-6">
-        <input type="reset" className="text-sm font-semibold leading-6 text-gray-900" value="Cancel"/>
-        <button
-          type="submit"
-          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          Generate
-        </button>
-      </div>
-    </form>
+
+
     </div>
   )
 }
